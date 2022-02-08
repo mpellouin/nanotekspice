4b7c5ae446325d10cf0cf840b6f1ff0e37d1975f
maxnce <maxence.pellouin@epitech.eu>
2022-02-08 11:27:57 +0100

    [Add] - Doxygen for parser class

--- inc/Parser.hpp
+++ inc/Parser.hpp
@@ -18,8 +18,26 @@ class Parser {
         Parser(const std::string &filepath);
         ~Parser();
 
+        /**
+         * @brief Get the Next Line object and stores it in parser class.
+         * 
+         */
         void getNextLine();
+
+        /**
+         * @brief Parse the next line from input.
+         * 
+         * In case of error, throws an exception
+         * @return Next parsable element as an std::string
+         */
         std::string parseLine();
+
+        /**
+         * @brief Indicates if next line is the beggining of a new
+         * section.
+         * 
+         * @return true if new section, false otherwise
+         */
         bool isNewSection();
 
     protected:

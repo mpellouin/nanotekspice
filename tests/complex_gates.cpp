9dcffd95a0f3c6605c57d847ff2b809a8f30a08d
Florian Gibault <florian.gibault@epitech.eu>
2022-02-16 13:36:25 +0100

    [FIX] 4069 bug resolved

--- tests/complex_gates.cpp
+++ tests/complex_gates.cpp
@@ -115,49 +115,41 @@ Test(Circuit, Circuit_4069_Gate)
     Circuit grid;
     grid.AddComponent("input", "A");
     grid.AddComponent("input", "B");
-    grid.AddComponent("4071", "C");
+    grid.AddComponent("4069", "C");
     grid.AddComponent("output", "D");
     grid.AddComponent("output", "E");
     grid.AddComponent("output", "F");
     grid.AddComponent("output", "G");
+    grid.AddComponent("output", "H");
+    grid.AddComponent("output", "I");
 
     grid.setLink(1, "A", 1, "C");
+    grid.setLink(1, "A", 3, "C");
     grid.setLink(1, "A", 5, "C");
-    grid.setLink(1, "A", 8, "C");
-    grid.setLink(1, "A", 12, "C");
-    grid.setLink(1, "B", 2, "C");
-    grid.setLink(1, "B", 6, "C");
     grid.setLink(1, "B", 9, "C");
+    grid.setLink(1, "B", 11, "C");
     grid.setLink(1, "B", 13, "C");
-    grid.setLink(3, "C", 1, "D");
-    grid.setLink(3, "C", 1, "E");
-    grid.setLink(3, "C", 1, "F");
-    grid.setLink(3, "C", 1, "G");
+    grid.setLink(2, "C", 1, "D");
+    grid.setLink(4, "C", 1, "E");
+    grid.setLink(6, "C", 1, "F");
+    grid.setLink(8, "C", 1, "G");
+    grid.setLink(10, "C", 1, "H");
+    grid.setLink(12, "C", 1, "I");
 
     dynamic_cast<Input *>(grid["A"])->setValue(nts::TRUE);
-    dynamic_cast<Input *>(grid["B"])->setValue(nts::TRUE);
+    dynamic_cast<Input *>(grid["B"])->setValue(nts::FALSE);
     grid.simulate(1);
-    cr_assert_eq(grid["D"]->compute(1), nts::TRUE);
-
-    dynamic_cast<Input *>(grid["A"])->setValue(nts::FALSE);
-    dynamic_cast<Input *>(grid["B"])->setValue(nts::TRUE);
-    grid.simulate(1);
-    cr_assert_eq(grid["E"]->compute(1), nts::TRUE);
-
-    dynamic_cast<Input *>(grid["A"])->setValue(nts::UNDEFINED);
-    dynamic_cast<Input *>(grid["B"])->setValue(nts::TRUE);
-    grid.simulate(1);
-    cr_assert_eq(grid["F"]->compute(1), nts::TRUE);
-
-    dynamic_cast<Input *>(grid["A"])->setValue(nts::FALSE);
-    dynamic_cast<Input *>(grid["B"])->setValue(nts::UNDEFINED);
-    grid.simulate(1);
-    cr_assert_eq(grid["G"]->compute(1), nts::UNDEFINED);
+    cr_assert_eq(grid["D"]->compute(1), nts::FALSE);
+    cr_assert_eq(grid["E"]->compute(1), nts::FALSE);
+    cr_assert_eq(grid["F"]->compute(1), nts::FALSE);
+    cr_assert_eq(grid["G"]->compute(1), nts::TRUE);
+    cr_assert_eq(grid["H"]->compute(1), nts::TRUE);
+    cr_assert_eq(grid["I"]->compute(1), nts::TRUE);
 
     try {
-        grid["G"]->compute(18);
+        grid["C"]->compute(18);
     } catch(const std::exception& e) {
-        cr_assert_str_eq(e.what(), "Pin not found");
+        cr_assert_str_eq(e.what(), "C4069: Pin not found");
     }
 }
 

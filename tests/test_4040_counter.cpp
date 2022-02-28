feffe44dd2c48478d40e8392e0aab73883147f9b
Florian Gibault <florian.gibault@epitech.eu>
2022-02-28 18:39:33 +0100

    [FEAT] Updated unit tests for counter

--- tests/test_4040_counter.cpp
+++ tests/test_4040_counter.cpp
@@ -26,22 +26,22 @@ Test(Circuit, C4040_component)
     grid.setLink(6, "counter", 1, "out3");
     grid.setLink(5, "counter", 1, "out4");
 
-    dynamic_cast<Input *>(grid["cl"])->setValue(nts::TRUE);
-    grid.simulate(1);
-    cr_assert_eq(grid["out1"]->compute(1), nts::UNDEFINED);
-
+    dynamic_cast<Input *>(grid["cl"])->setValue(nts::FALSE);
     grid.simulate(1);
     cr_assert_eq(grid["out1"]->compute(1), nts::UNDEFINED);
 
     dynamic_cast<Input *>(grid["in1"])->setValue(nts::TRUE);
     grid.simulate(1);
-    cr_assert_eq(grid["counter"]->compute(1), nts::FALSE);
+    cr_assert_eq(grid["out1"]->compute(1), nts::FALSE);
 
     dynamic_cast<Input *>(grid["in1"])->setValue(nts::FALSE);
     grid.simulate(1);
-    grid.simulate(1);
-    grid.simulate(1);
     cr_assert_eq(grid["out1"]->compute(1), nts::TRUE);
+
+    grid.simulate(1);
+    grid.simulate(1);
+    cr_assert_eq(grid["out1"]->compute(1), nts::FALSE);
+    cr_assert_eq(grid["out2"]->compute(1), nts::TRUE);
 
     try {
         grid["counter"]->compute(19);
